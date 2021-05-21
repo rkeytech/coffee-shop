25ba0fcf61e470a563d961c4bd1c8ea278df70a8
Timos Zacharatos <rkeytech@protonmail.com>
2021-05-21 21:01:00 +0300

    errors/static

---
+++ RESOURCES.md
@@ -0,0 +1,6 @@
+# Resources
+## Main Libraries
+* [Flask](https://flask.palletsprojects.com/en/2.0.x/tutorial/)
+* [Jinja](https://jinja2docs.readthedocs.io/en/stable/)
+* [Flask WTF](https://flask-wtf.readthedocs.io/en/stable/)
+* [Werkzeug](https://werkzeug.palletsprojects.com/en/2.0.x/)
--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+
+# Create a Flask Instance
 app = Flask(__name__)
+# Create a database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafe.db'
+# Create a secret for form validation
+app.config['SECRET_KEY'] = 'not so much secure key'
 
 
+# Basic Routing
 @app.route('/')
 def dashboard():
     return render_template('dashboard.html', title='Dashboard')
@@ -10,20 +22,37 @@ def dashboard():
 
 @app.route('/stuff')
 def stuff():
-    stuff = [["Timos", "Zach", "Waiter"],
-             ["Giorgos", "Zach", "Waiter"],
-             ["Eva", "Zach", "Waiter"]]
-    return render_template('stuff.html', stuff=stuff)
+    stuff = [[0, "Timos", "Zach", "Waiter"],
+             [1, "Giorgos", "Zach", "Waiter"],
+             [2, "Eva", "Zach", "Waiter"]]
+    return render_template('stuff.html', stuff=stuff, title='Stuff')
 
 
-@app.route('/products')
+@app.route('/profile/<pk>')
+def profile(pk):
+    context = [0, "Timos", "Zach", "Waiter"]
+    return render_template('profile.html', title=context[1], context=context)
+
+
+@ app.route('/products')
 def products():
     return render_template('products.html', title='Products')
 
 
-@app.route('/orders')
+@ app.route('/orders')
 def orders():
     orders = [["1", "frappe", "3.40", "Timos", "01/02/2020"],
               ["2", "tost", "1.30", "Eva", "01/03/2020"],
               ["3", "freddo", "4.10", "Timos", "01/01/2020"]]
-    return render_template('orders.html', orders=orders)
+    return render_template('orders.html', orders=orders, title='Orders')
+
+
+# Custom Error Pages
+@ app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', title='404 Error'), 404
+
+
+@ app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html', title='500 Error'), 500
