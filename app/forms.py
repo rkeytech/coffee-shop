a4e2f2fca4f575a400072fc3b16afd3c8ca019c6
Timos Zacharatos <rkeytech@protonmail.com>
2021-06-15 19:33:13 +0300

    Changed project structure

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DecimalField
+from wtforms import StringField, SubmitField, DecimalField, PasswordField
 from wtforms.validators import DataRequired, Email
 
 
@@ -8,6 +8,8 @@ class UserForm(FlaskForm):
     last_name = StringField('Last Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     phone = StringField('Phone', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
     job = StringField('Job', validators=[DataRequired()])
     wage = DecimalField('Wage', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
-from app.database import db
+from . import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(120), nullable=False)
     last_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
-    phone = db.Column(db.String(50), default="No number")
+    phone = db.Column(db.String(50), nullable=False, default="No number")
+    address = db.Column(db.String(50), nullable=False, default="No Address")
+    password = db.Column(db.String(150), nullable=False)
     job = db.Column(db.String(50), nullable=False)
     wage = db.Column(db.Float, nullable=False)
 
